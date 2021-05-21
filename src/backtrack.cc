f8e1e68137c92558b0e052d2956ec9be3bb058ab
JunYoung <702wnsdud@gmail.com>
2021-05-21 22:33:01 +0900

    time checker

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -4,6 +4,7 @@
  */
 
 #include "backtrack.h"
+#include <chrono>
 
 Backtrack::Backtrack() {}
 
@@ -14,7 +15,11 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
   int num = query.GetNumVertices();
   std::vector<int> ans(num, -1);
   std::cout << "t " << num << "\n";
+  std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
   NaiveCheck(data, query, cs, 0, num, ans);
+  std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+  std::chrono::duration<double> time = end - start;
+  std::cout << "Elapsed Time: " << time.count() << "s\n";
 }
 
 void Backtrack::PrintCandidates(std::vector<int> &ans) {
@@ -33,36 +38,33 @@ bool Backtrack::CheckNeighbors(const Graph &data, const Graph &query, const Cand
   bool temp = false;
   int queryNbhStart = query.GetNeighborStartOffset(index);
   int queryNbhEnd = query.GetNeighborEndOffset(index);
-  int csNbhStart = data.GetNeighborStartOffset(csIndex);
-  int csNbhEnd = data.GetNeighborEndOffset(csIndex);
+//  int csNbhStart = data.GetNeighborStartOffset(csIndex);
+//  int csNbhEnd = data.GetNeighborEndOffset(csIndex);
   for (int _i = queryNbhStart; _i < queryNbhEnd; ++_i) {
     temp = false;
-/*
-    int queryNbhLabel = query.GetLabel(query.GetNeighbor(_i));
-    for (int _j = csNbhStart; _j < csNbhEnd; ++_j) {
-      if (queryNbhLabel == data.GetLabel(data.GetNeighbor(_j))) {
-        temp = true;
-        break;
-      }
-    }
-*/
+//    int queryNbhLabel = query.GetLabel(query.GetNeighbor(_i));
+//    for (int _j = csNbhStart; _j < csNbhEnd; ++_j) {
+//      if (queryNbhLabel == data.GetLabel(data.GetNeighbor(_j))) {
+//        temp = true;
+//        break;
+//      }
+//    }
 // query의 이웃의 Candidate만 확인하면 됨
 // 개선방안: Dynamic Programming으로 이전에 확인했을때 실패한 노드는 확인안하기
 // (성공한 노드를 배열에 저장해놓고 그 안에서만 확인, 매번 업데이트)
 // (+ Neighbor에 대해서도 비슷한 방법 사용가능할듯)
     int ithNbh = query.GetNeighbor(_i);
     int queryNbhCsSize = cs.GetCandidateSize(ithNbh);
-    for (int _j = 0; _j < queryNbhCsSize; ++_j)
-    {
-      if (data.IsNeighbor(csIndex, cs.GetCandidate(ithNbh, _j)))
-      {
+    for (int _j = 0; _j < queryNbhCsSize; ++_j) {
+      if (data.IsNeighbor(csIndex, cs.GetCandidate(ithNbh, _j))) {
         temp = true;
         break;
       }
     }
-    ans = ans && temp;
+    if (!temp)
+      return false;
   }
-  return ans;
+  return true;
 }
 
 // Graph에 label frequency를 구할 수 있는 함수가 있음, 이걸로 좀 더 효율적인 검색을 할 수 있을듯?
@@ -71,7 +73,7 @@ void Backtrack::NaiveCheck(const Graph &data, const Graph &query, const Candidat
   if (index == size) {
     PrintCandidates(acc);
     /* 제출 시 checkAnswer는 comment out */
-    checkAnswer(acc, data, query);
+//    checkAnswer(acc, data, query);
     return;
   } else {
     int csNum = cs.GetCandidateSize(index);
@@ -94,16 +96,17 @@ void Backtrack::NaiveCheck(const Graph &data, const Graph &query, const Candidat
   }
 }
 
-void Backtrack::checkAnswer(std::vector<int> &acc, const Graph &data, const Graph &query){
+void Backtrack::checkAnswer(std::vector<int> &acc, const Graph &data, const Graph &query) {
   for (int i = 0; i < query.GetNumVertices(); i++) {
     /* 먼저 vertext label이 같은지 체크 */
-    if(query.GetLabel(i) != data.GetLabel(acc[i])) {
+    if (query.GetLabel(i) != data.GetLabel(acc[i])) {
       std::cout << "========== " << i << "th vertex label does not match! ========== " << std::endl;
     }
     /* data graph와 query graph 사이의 edge 관계가 같은지 체크: query graph에 edge가 있다면 data graph에도 edge가 있어야*/
-    for(int j = i + 1; j < query.GetNumVertices(); j++){
-      if(query.IsNeighbor(i, j) && !data.IsNeighbor(acc[i], acc[j])){
-        std::cout << "========== " << acc[i] << " and " << acc[j] << " does not have edge on data graph ========== "<< std::endl;
+    for (int j = i + 1; j < query.GetNumVertices(); j++) {
+      if (query.IsNeighbor(i, j) && !data.IsNeighbor(acc[i], acc[j])) {
+        std::cout << "========== " << acc[i] << " and " << acc[j] << " does not have edge on data graph ========== "
+                  << std::endl;
       }
     }
   }
